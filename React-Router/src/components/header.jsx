7d63ccfbddc11e834304f09c985a35e060d62bab
Hammad890 <hammadbro6@gmail.com>
2025-01-09 01:02:38 +0500

    feat: wrap children in layout

---
+++ React-Router/src/components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">My Website</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><a href="/" className="hover:text-blue-300">Home</a></li>
+            <li><a href="/about" className="hover:text-blue-300">About</a></li>
+            <li><a href="/profile/id" className="hover:text-blue-300">Profile</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- React-Router/src/router/index.js
+++ React-Router/src/router/index.js
@@ -3,21 +3,19 @@ import About from "../pages/about";
 import Home from "../pages/home";
 import Profile from "../pages/profile";
 import ErrorPage from "../pages/error-page";
+import Layout from "../components/layout";
 
 
 const router = createBrowserRouter([
-  { path: "/",  
-    element: <Home/>, 
-    errorElement: <ErrorPage/>
-},
-  { path: "/about", 
-    element: <About/>,
-    errorElement: <ErrorPage/>
-},
-  { path: "/profile/:id", 
-    element: <Profile/>, 
-    errorElement: <ErrorPage/>
-},
+  { path: "/", 
+  element: <Layout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      { path: "/", index: true, element: <Home/> },
+      { path: "about", element: <About/> },
+      { path: "profile/:id", element: <Profile/> }
+    ]
+    }
 ])
 
 export default router;
