ca4c3738764bca855f0366d99b6fbe79ad8925cd
Hammad890 <hammadbro6@gmail.com>
2025-01-09 01:17:52 +0500

    updated header file

--- React-Router/src/components/header.jsx
+++ React-Router/src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Header() {
+    const id = 1
   return (
     <header className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -9,7 +10,7 @@ function Header() {
           <ul className="flex space-x-4">
             <li><a href="/" className="hover:text-blue-300">Home</a></li>
             <li><a href="/about" className="hover:text-blue-300">About</a></li>
-            <li><a href="/profile/id" className="hover:text-blue-300">Profile</a></li>
+            <li><a href={`/profile/${id}`} className="hover:text-blue-300">Profile</a></li>
           </ul>
         </nav>
       </div>
